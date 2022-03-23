41df15d7e5f501491e029996beb3b09a90c8faa9
Sinkaravel097 <loophawk721@gmail.com>
2022-03-23 07:15:38 +0530

    emp A new work

--- src/org/code/SnapDeal.java
+++ src/org/code/SnapDeal.java
@@ -5,5 +5,8 @@ public class SnapDeal {
 	private void empA() {
 		System.out.println("emp a work is done");
 	}
+	private void empA2() {
+		System.out.println("emp a working in ne function");
+	}
 
 }
